3f49a72042b3b00a923a4e42768530da8fddb2dd
eitanLieberman <eitanl95@gmail.com>
2021-12-27 19:25:47 +0200

    cart routes done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import { default as userRoute } from "./routes/user.js";
 import { default as authRoute } from "./routes/auth.js";
 import { default as productRoute } from "./routes/product.js";
+import { default as cartRoute } from "./routes/product.js";
 dotenv.config();
 const app = express();
 //check
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/cart", cartRoute);
 app.listen(process.env.PORT || 8080, () => {
   console.log("backend server running!");
 });
