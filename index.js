640a633d3e8076d23d83f17229cf2b54b2d388a5
eitanLieberman <eitanl95@gmail.com>
2021-12-27 18:48:21 +0200

    Produc routes started

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { default as userRoute } from "./routes/user.js";
 import { default as authRoute } from "./routes/auth.js";
-
+import { default as productRoute } from "./routes/product.js";
 dotenv.config();
 const app = express();
 //check
@@ -17,7 +17,7 @@ try {
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
-
+app.use("/api/products", productRoute);
 app.listen(process.env.PORT || 8080, () => {
   console.log("backend server running!");
 });
