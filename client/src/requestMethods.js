b53b36c1a82338be2afc024ca8552fe1575c3431
eitanLieberman <eitanl95@gmail.com>
2022-01-04 14:05:50 +0200

    moving the fronted to the server

---
+++ client/src/requestMethods.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:8080/api/";
+
+const Token =
+  JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
+    ?.accessToken || null;
+
+console.log(Token);
+export const publicRequest = axios.create({ baseURL: BASE_URL });
+
+export const userRequest = axios.create({
+  baseURL: BASE_URL,
+  headers: { token: `Bearer ${Token}` },
+});
